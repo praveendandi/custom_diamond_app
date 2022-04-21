27ef86fcba87cef8cdf5aaddf978523cadb4cc32
kiran <kiran@caratred.com>
2022-04-21 15:55:21 +0550

    feat: Initialize App

---
+++ custom_diamond_app/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Custom Diamond App",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Custom Diamond App")
+		}
+	]
