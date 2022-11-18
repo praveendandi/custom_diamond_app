28ddfbcdd881b7581efa85bf7f7a024d6b3e22af
Ganu Reddy <ganureddy54@gmail.com>
2022-11-18 11:25:45 +0530

    bank report

--- custom_diamond_app/custom_diamond_app/report/bank_account/bank_account.py
+++ custom_diamond_app/custom_diamond_app/report/bank_account/bank_account.py
@@ -103,7 +103,7 @@ class AccountsReceivableSummary(ReceivablePayableReport):
 		self.columns = []
 		if self.party_naming_by == "Naming Series":
 			self.add_column(_("Bank Account Number"),fieldname="bank_account_number", fieldtype="Int")
-			self.add_column(_("Outstanding Amount"), fieldname="outstanding")
+			self.add_column(_("Amount"), fieldname="outstanding")
 			self.add_column(_("{0} Name").format(self.party_type), fieldname="party_name", fieldtype="Data")
 			self.add_column(label=_(self.party_type),fieldname="party",fieldtype="Link",
 						options=self.party_type,width=180,)
