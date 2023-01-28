ce8ed588168254567f8f51881b1ab8a8dcbb1bbe
Ganu Reddy <ganu.b@caratred.com>
2023-01-28 11:21:23 +0530

    created empty columns in bank report

--- custom_diamond_app/custom_diamond_app/report/bank_account/bank_account.js
+++ custom_diamond_app/custom_diamond_app/report/bank_account/bank_account.js
@@ -1,65 +1,65 @@
-// Copyright (c) 2022, Ganu and contributors
+// Copyright (c) 2022, Ganu Reddy and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
 frappe.query_reports["Bank Account"] = {
 	"filters": [
 		{
-			"fieldname": "company",
+			"fieldname":"company",
 			"label": __("Company"),
 			"fieldtype": "Link",
 			"options": "Company",
 			"default": frappe.defaults.get_user_default("Company")
 		},
 		{
-			"fieldname": "report_date",
+			"fieldname":"report_date",
 			"label": __("Posting Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today()
 		},
 		{
-			"fieldname": "ageing_based_on",
+			"fieldname":"ageing_based_on",
 			"label": __("Ageing Based On"),
 			"fieldtype": "Select",
 			"options": 'Posting Date\nDue Date',
 			"default": "Due Date"
 		},
 		{
-			"fieldname": "range1",
+			"fieldname":"range1",
 			"label": __("Ageing Range 1"),
 			"fieldtype": "Int",
 			"default": "30",
 			"reqd": 1
 		},
 		{
-			"fieldname": "range2",
+			"fieldname":"range2",
 			"label": __("Ageing Range 2"),
 			"fieldtype": "Int",
 			"default": "60",
 			"reqd": 1
 		},
 		{
-			"fieldname": "range3",
+			"fieldname":"range3",
 			"label": __("Ageing Range 3"),
 			"fieldtype": "Int",
 			"default": "90",
 			"reqd": 1
 		},
 		{
-			"fieldname": "range4",
+			"fieldname":"range4",
 			"label": __("Ageing Range 4"),
 			"fieldtype": "Int",
 			"default": "120",
 			"reqd": 1
 		},
 		{
-			"fieldname": "finance_book",
+			"fieldname":"finance_book",
 			"label": __("Finance Book"),
 			"fieldtype": "Link",
 			"options": "Finance Book"
 		},
 		{
-			"fieldname": "cost_center",
+			"fieldname":"cost_center",
 			"label": __("Cost Center"),
 			"fieldtype": "Link",
 			"options": "Cost Center",
@@ -73,36 +73,43 @@ frappe.query_reports["Bank Account"] = {
 			}
 		},
 		{
-			"fieldname": "supplier",
+			"fieldname":"supplier",
 			"label": __("Supplier"),
 			"fieldtype": "Link",
 			"options": "Supplier"
 		},
 		{
-			"fieldname": "payment_terms_template",
+			"fieldname":"payment_terms_template",
 			"label": __("Payment Terms Template"),
 			"fieldtype": "Link",
 			"options": "Payment Terms Template"
 		},
 		{
-			"fieldname": "supplier_group",
+			"fieldname":"supplier_group",
 			"label": __("Supplier Group"),
 			"fieldtype": "Link",
 			"options": "Supplier Group"
 		},
 		{
-			"fieldname": "based_on_payment_terms",
+			"fieldname":"based_on_payment_terms",
 			"label": __("Based On Payment Terms"),
 			"fieldtype": "Check",
+		},
+		{
+			"fieldname":"empty_columns",
+			"label": __("Empty Columns"),
+			"fieldtype": "Check",
+			"default": 0
 		}
 	],
 
-	onload: function (report) {
-		report.page.add_inner_button(__("Accounts Payable"), function () {
+	onload: function(report) {
+		report.page.add_inner_button(__("Accounts Payable"), function() {
 			var filters = report.get_values();
-			frappe.set_route('query-report', 'Accounts Payable', { company: filters.company });
+			frappe.set_route('query-report', 'Accounts Payable', {company: filters.company});
 		});
 	}
 };
+
 
 erpnext.utils.add_dimensions('Bank Account', 9);
