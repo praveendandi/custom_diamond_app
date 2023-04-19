b7490d77f9def49e091555063a25ad455a70cea1
girishjaiswal1993 <girish.jaiswal@caratred.com>
2023-04-19 18:53:15 +0530

    initial bug fixed

--- custom_diamond_app/events.py
+++ custom_diamond_app/events.py
@@ -598,59 +598,59 @@ def bank_transaction(data,method=None):
 
 
 
-@frappe.whitelist()
-def make_work_order(source_name, target_doc=None, args=None):
-    print(source_name,target_doc,args,"////////////")
-    # if args is None:
-    #     args = {}
-    # if isinstance(args, string_types):
-    #     args = json.loads(args)
+# @frappe.whitelist()
+# def make_work_order(source_name, target_doc=None, args=None):
+#     print(source_name,target_doc,args,"////////////")
+#     # if args is None:
+#     #     args = {}
+#     # if isinstance(args, string_types):
+#     #     args = json.loads(args)
 
-    # def postprocess(source, target_doc):
-    #     if frappe.flags.args and frappe.flags.args.default_supplier:
-    #         # items only for given default supplier
-    #         supplier_items = []
-    #         for d in target_doc.items:
-    #             default_supplier = get_item_defaults(d.item_code, target_doc.company).get("default_supplier")
-    #             if frappe.flags.args.default_supplier == default_supplier:
-    #                 supplier_items.append(d)
-    #         target_doc.items = supplier_items
+#     # def postprocess(source, target_doc):
+#     #     if frappe.flags.args and frappe.flags.args.default_supplier:
+#     #         # items only for given default supplier
+#     #         supplier_items = []
+#     #         for d in target_doc.items:
+#     #             default_supplier = get_item_defaults(d.item_code, target_doc.company).get("default_supplier")
+#     #             if frappe.flags.args.default_supplier == default_supplier:
+#     #                 supplier_items.append(d)
+#     #         target_doc.items = supplier_items
 
-    #     set_missing_values(source, target_doc)
+#     #     set_missing_values(source, target_doc)
 
-    # def select_item(d):
-    #     filtered_items = args.get("filtered_children", [])
-    #     child_filter = d.name in filtered_items if filtered_items else True
+#     # def select_item(d):
+#     #     filtered_items = args.get("filtered_children", [])
+#     #     child_filter = d.name in filtered_items if filtered_items else True
 
-    #     return d.ordered_qty < d.stock_qty and child_filter
+#     #     return d.ordered_qty < d.stock_qty and child_filter
 
-    # doclist = get_mapped_doc(
-    #     "Work Order",
-    #     source_name,
-    #     {
-    #         "Work Order": {
-    #             "doctype": "Purchase Order",
-    #             "validation": {"docstatus": ["=", 1], "material_request_type": ["=", "Purchase"]},
-    #         },
-    #         "Material Request Item": {
-    #             "doctype": "Purchase Order Item",
-    #             "field_map": [
-    #                 ["name", "material_request_item"],
-    #                 ["parent", "material_request"],
-    #                 ["uom", "stock_uom"],
-    #                 ["uom", "uom"],
-    #                 ["sales_order", "sales_order"],
-    #                 ["sales_order_item", "sales_order_item"],
-    #             ],
-    #             "postprocess": update_item,
-    #             "condition": select_item,
-    #         },
-    #     },
-    #     target_doc,
-    #     postprocess,
-    # )
+#     # doclist = get_mapped_doc(
+#     #     "Work Order",
+#     #     source_name,
+#     #     {
+#     #         "Work Order": {
+#     #             "doctype": "Purchase Order",
+#     #             "validation": {"docstatus": ["=", 1], "material_request_type": ["=", "Purchase"]},
+#     #         },
+#     #         "Material Request Item": {
+#     #             "doctype": "Purchase Order Item",
+#     #             "field_map": [
+#     #                 ["name", "material_request_item"],
+#     #                 ["parent", "material_request"],
+#     #                 ["uom", "stock_uom"],
+#     #                 ["uom", "uom"],
+#     #                 ["sales_order", "sales_order"],
+#     #                 ["sales_order_item", "sales_order_item"],
+#     #             ],
+#     #             "postprocess": update_item,
+#     #             "condition": select_item,
+#     #         },
+#     #     },
+#     #     target_doc,
+#     #     postprocess,
+#     # )
 
-    # return doclist
+#     # return doclist
       
         
         
